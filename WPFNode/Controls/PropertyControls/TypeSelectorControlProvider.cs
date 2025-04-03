86ab6a59ef4fb479ce6e15497ff34f79fa389861
KwonSangGoo <milennium9@naver.com>
2025-04-03 21:49:28 +0900

    타입 선택기 컨트롤을 위한 TypeValueConverter 및 TypePropertyWrapper 클래스를 추가하여 타입 바인딩을 개선하였습니다. TypeValueConverter는 타입 변환을 처리하며, TypePropertyWrapper는 INodeProperty의 변경을 감지하여 바인딩된 값을 업데이트합니다. 이를 통해 타입 선택기의 동작을 보다 안정적이고 유연하게 개선하였습니다.

--- WPFNode/Controls/PropertyControls/TypeSelectorControlProvider.cs
+++ WPFNode/Controls/PropertyControls/TypeSelectorControlProvider.cs
@@ -3,8 +3,25 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using WPFNode.Interfaces;
 using WPFNode.Controls;
+using System;
+using System.Globalization;
+using System.ComponentModel;
 
 namespace WPFNode.Controls.PropertyControls;
+
+// Type 변환을 위한 컨버터 클래스
+public class TypeValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value as Type;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
 
 public class TypeSelectorControlProvider : IPropertyControlProvider
 {
@@ -14,17 +31,62 @@ public class TypeSelectorControlProvider : IPropertyControlProvider
     {
         var typeSelector = new TypeSelector();
         
-        var binding = new Binding("Value")
+        // NodeProperty 클래스의 명확한 Type 값을 바인딩하기 위한 특수 래퍼 생성
+        var propertyWrapper = new TypePropertyWrapper(property);
+        
+        var binding = new Binding("TypeValue")
         {
-            Source = property,
+            Source = propertyWrapper,
             Mode = BindingMode.TwoWay,
             UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
         };
+        
         typeSelector.SetBinding(TypeSelector.SelectedTypeProperty, binding);
+        
+        // 래퍼 객체를 태그에 저장하여 GC에 의해 회수되지 않도록 함
+        typeSelector.Tag = propertyWrapper;
         
         return typeSelector;
     }
     
     public int Priority => 0;
     public string ControlTypeId => "TypeSelector";
+}
+
+// NodeProperty 바인딩을 위한 래퍼 클래스
+public class TypePropertyWrapper : INotifyPropertyChanged
+{
+    private readonly INodeProperty _property;
+    
+    public TypePropertyWrapper(INodeProperty property)
+    {
+        _property = property;
+        // 원본 프로퍼티의 변경 감지
+        _property.PropertyChanged += (s, e) => {
+            if (e.PropertyName == "Value")
+            {
+                OnPropertyChanged(nameof(TypeValue));
+            }
+        };
+    }
+    
+    public Type TypeValue
+    {
+        get => _property.Value as Type;
+        set
+        {
+            if (_property.Value != value)
+            {
+                _property.Value = value;
+                OnPropertyChanged(nameof(TypeValue));
+            }
+        }
+    }
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 } 
