bc068e033929626078c33391004de64d2624a447
KwonSangGoo <milennium9@naver.com>
2025-06-02 16:32:46 +0900

    CreateObjectNode에서 Indexer에 값 할당 중 Exception 발생 문제 수정.

--- WPFNode.Plugins.Basic/Nodes/CreateObjectNode.cs
+++ WPFNode.Plugins.Basic/Nodes/CreateObjectNode.cs
@@ -52,7 +52,7 @@ namespace WPFNode.Plugins.Basic.Nodes {
 
             // 타겟 타입의 쓰기 가능한 속성과 필드들 가져오기
             var targetProperties = targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                             .Where(p => p.CanWrite)
+                                             .Where(p => p.CanWrite && p.GetIndexParameters().Length == 0)
                                              .ToList();
 
             var targetFields = targetType.GetFields(BindingFlags.Public | BindingFlags.Instance)
